b59a6e42cb867fd30daf1b2101afd0d5dd582f10
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-05 16:05:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,3 +5,4 @@ board = [" "," "," "," "," "," "," "," "," "]
 def display_board board
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
+display_board
